9f058d955404450585b82f44d15a96a5c5997a20
Ahmad Shahzad <capsyz@outlook.com>
2023-03-11 10:12:02 +0500

    Separated the routes into separate modules.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 4500;
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
-});
+const router = {
+    home: require(path.join(__dirname, 'routes', 'home')),
+    error: require(path.join(__dirname, 'routes', '404')),
+};
+
+app.use('/', router.home);
+app.use('*', router.error);
 
 app.listen(port, () => {
     console.log(
