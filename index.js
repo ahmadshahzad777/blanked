6abe123c7211ecf8dcfd67e93e70bdf4cba21e75
Ahmad Shahzad <capsyz@outlook.com>
2023-03-14 13:00:57 +0500

    Setup body-parser to manage form body.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 4500;
@@ -12,6 +13,9 @@ nunjucks.configure(path.join(__dirname, 'views'), {
 app.set('view engine', 'html');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const router = {
     home: require(path.join(__dirname, 'routes', 'home')),
