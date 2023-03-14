b0b91dc8ac0ae4ad3a9740596db94eae2d18b758
Ahmad Shahzad <capsyz@outlook.com>
2023-03-14 14:06:31 +0500

    Created the logic to clean the link as the main user.

--- routes/clean.js
+++ routes/clean.js
@@ -8,21 +8,65 @@ const router = express.Router();
 module.exports = router;
 
 router.post('/', (req, res) => {
-    db.query('SELECT * FROM users', (err, rows) => {
-        if (err) {
-            console.error('There was an error.', err);
-            return res.json({ status: false, alias: null });
-        } else {
-            const o_id = 0;
-            const link = req.body.link;
-            const alias = nanoid(12);
-            const track = true;
-            const password = '';
-            const views = 0;
-            const enabled = true;
-            const created_on = new Date().toISOString();
+    const link = req.body.link;
+    const o_id = 1;
 
-            return res.json({ status: true, alias });
-        }
-    });
+    db.query(
+        `SELECT * FROM links WHERE o_id = ${o_id} AND link = "${link}"`,
+        (err, rows) => {
+            if (err) {
+                console.log(
+                    'There was an error getting the links from the database.',
+                    err,
+                );
+                return res.json({
+                    status: false,
+                    alias: null,
+                    code: 'ERROR1',
+                    message: err,
+                });
+            } else {
+                if (rows.length > 0) {
+                    return res.json({
+                        status: true,
+                        alias: rows[0].alias,
+                        code: 'SUCCESS',
+                        message: null,
+                    });
+                } else {
+                    const alias = nanoid(12);
+                    const track = true;
+                    const password = '';
+                    const views = 0;
+                    const enabled = true;
+                    const created_on = new Date().toISOString();
+
+                    db.query(
+                        `INSERT INTO links (o_id, link, alias, track, password, views, enabled, created_on) VALUES (${o_id}, "${link}", "${alias}", ${track}, "${password}", ${views}, ${enabled}, "${created_on}")`,
+                        (err) => {
+                            if (err) {
+                                console.error(
+                                    'There was an error creating the link in the database.',
+                                    err,
+                                );
+                                return res.json({
+                                    status: false,
+                                    alias: null,
+                                    code: 'ERROR2',
+                                    message: err,
+                                });
+                            } else {
+                                return res.json({
+                                    status: true,
+                                    alias,
+                                    code: 'SUCCESS',
+                                    message: null,
+                                });
+                            }
+                        },
+                    );
+                }
+            }
+        },
+    );
 });
