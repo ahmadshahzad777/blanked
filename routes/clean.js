24f6b5ba30c77c099008d0d39fb187eb9d5685c6
Ahmad Shahzad <capsyz@outlook.com>
2023-03-14 13:14:58 +0500

    Created a dummy clean link route.

--- routes/clean.js
+++ routes/clean.js
@@ -1,7 +1,19 @@
+const path = require('path');
 const express = require('express');
+
+const db = require(path.join(__dirname, '..', 'db'));
+
 const router = express.Router();
 module.exports = router;
 
 router.post('/', (req, res) => {
-    return res.json({ status: true });
+    db.query('SELECT * FROM users', (err, rows) => {
+        if (err) {
+            console.error('There was an error.', err);
+            return res.json({ status: false, link: null });
+        } else {
+            const { link } = req.body;
+            return res.json({ status: true, link });
+        }
+    });
 });
