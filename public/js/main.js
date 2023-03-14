bd372afdd4cc86274f0515adbb4ead8c80a70508
Ahmad Shahzad <capsyz@outlook.com>
2023-03-14 15:10:50 +0500

    Finalized the link copier.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,10 @@
 const link = document.getElementById('link');
 const clean = document.getElementById('clean');
 const cleanit = document.getElementById('cleanit');
+const newlink = document.getElementById('newlink');
+const thelink = document.getElementById('thelink');
+const copier = document.getElementById('copy');
+const closelink = document.getElementById('closelink');
 
 clean.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -8,7 +12,7 @@ clean.addEventListener('submit', (e) => {
 
 cleanit.addEventListener('click', async (e) => {
     e.preventDefault();
-    if (isValidUrl(link.value)) {
+    if (isValidUrl(link.value) && link.value !== '') {
         const response = await fetch('/clean', {
             method: 'POST',
             headers: {
@@ -19,12 +23,25 @@ cleanit.addEventListener('click', async (e) => {
 
         const data = await response.json();
 
-        console.log(data);
+        if (data.status === true) {
+            thelink.innerText = `https://blanked.link/${data.alias}`;
+            newlink.classList.remove('hidden');
+        }
     } else {
         alert(
             'Please enter a valid URL of the format: http(s)://(www.)example.com',
         );
     }
+});
+
+copier.addEventListener('click', () => {
+    navigator.clipboard.writeText(thelink.innerText);
+    alert('Successfully copied the link to your clipboard.');
+});
+
+closelink.addEventListener('click', () => {
+    newlink.classList.add('hidden');
+    link.value = '';
 });
 
 // https://www.freecodecamp.org/news/check-if-a-javascript-string-is-a-url
